e881f01239694bc829c8e9f03d9b95814dd72884
Ruda <ruda.rabello.silva@gmail.com>
2022-07-22 11:44:01 -0300

    feat: gamepost

--- boardcamp-back/src/controllers/gamesController.js
+++ boardcamp-back/src/controllers/gamesController.js
@@ -1,40 +1,46 @@
 import connection from "../databases/postgres.js";
+import { gameModel } from "../models/gameModel.js";
 
-export async function addGame(req,res){
-    try {
-        const checkCategory = await connection.query(`
-        SELECT * FROM categories WHERE id=${req.body.categoryId}`);
-
-        if(!checkCategory) return res.status(400).send({message:'Categoria não cadastrada!'});
-
-        const checkGame = await connection.query(`
-        SELECT * FROM games WHERE name=${req.body.name}`);
-
-        if(!checkGame) return res.status(400).send({message:'Este jogo já está cadastrado!'});
-
-        await connection.query(
-            `INSERT INTO games (name,image,stockTotal,categoryId,pricePerDay,categoryName)
-            VALUES ('Banco Imobiliário','http://',3,1,1500,${checkCategory.name})`);
-
-        res.status(201).send('Jogo Cadastrado!')
-    } catch (error) {
-        res.sendStatus(500);
-    }
-}
-
-export async function getGames(req,res){
+export async function getGames(req, res) {
     let { name } = req.query;
     try {
-        
-        if(name){
-            const { rows:games } = await connection.query(
+        if (name) {
+            const { rows: games } = await connection.query(
                 `SELECT * FROM games WHERE name=${name}`);
             return res.status(200).send(games);
         }
-
-        const { rows:games } = await connection.query(`SELECT * FROM games`);
+        const { rows: games } = await connection.query(
+            `SELECT * FROM games`);
         res.status(200).send(games);
     } catch (error) {
         res.sendStatus(500);
     }
 }
+
+export async function addGame(req, res) {
+    const game = req.body;
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    const { error } = gameModel.validate(game);
+    if (error) {
+        res.sendStatus(400);
+    }
+    const checkCategory = await connection.query(`
+        SELECT * FROM categories WHERE id = $1`, [categoryId]);
+    if (!checkCategory) {
+        res.sendStatus(400);
+    }
+    const checkName = await connection.query(`
+        SELECT * FROM games WHERE name = $1`, [name]);
+    if (!checkName) {
+        res.sendStatus(409);
+    }
+    try {
+        await connection
+            .query(`INSERT INTO games ("name", "image", "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)`,
+                [name, image, stockTotal, categoryId, pricePerDay]);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500);
+    }
+}
+
--- boardcamp-back/src/routes/gameRouter.js
+++ boardcamp-back/src/routes/gameRouter.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import gameModel from '../models/gameModel.js';
 import { addGame, getGames } from '../controllers/gamesController.js';
 
 const gameRouter = Router();
 
 gameRouter.get('/games',getGames);
-gameRouter.post('/games',gameModel,addGame);
+gameRouter.post('/games',addGame);
 
 export default gameRouter;
