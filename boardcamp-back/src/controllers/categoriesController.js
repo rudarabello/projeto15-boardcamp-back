f5d67dc9797162f6d2d65466a04379bd9040e4f9
Ruda <ruda.rabello.silva@gmail.com>
2022-07-22 09:48:16 -0300

    fix: categories

--- boardcamp-back/src/controllers/categoriesController.js
+++ boardcamp-back/src/controllers/categoriesController.js
@@ -1,21 +1,29 @@
 import connection from "../databases/postgres.js";
 
-export async function getCategories(req,res){
+export async function getCategories(req, res) {
     try {
-        const {rows: categories} = await connection.query('SELECT * FROM categories');
+        const { rows: categories } = await connection.query(
+            'SELECT * FROM categories');
         res.status(200).send(categories);
     } catch (error) {
         res.sendStatus(500);
     }
 }
 
-export async function addCategory(req,res){
+export async function addCategory(req, res) {
+    const name = req.body.name;
     try {
-        const check = await connection.query(`SELECT * FROM categories WHERE name=${req.body}`);
-        if(check) return res.status(409).send({message:'Categoria já cadastrada!'});
-        await connection.query(`INSERT INTO categories (name) VALUES ('Terror')`);
-        res.status(201).send('Inserido');
+        const check = await connection.query(
+            'SELECT name FROM categories WHERE name = $1', [name])
+        if (check.rows.length !== 0) {
+            res.sendStatus(409);
+        } else {
+            await connection.query(
+                'INSERT INTO categories (name) VALUES ($1)', [name]);
+            res.sendStatus(201);
+        }
     } catch (error) {
+        console.log(error)
         res.sendStatus(500);
     }
 }
